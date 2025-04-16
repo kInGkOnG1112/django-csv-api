81419eb62db0749ac7347bd3b96ab0a5c0496590
Harold king Talip <htalip@traxiontech.net>
2025-04-16 14:58:23 +0800

    Initialize project

---
+++ app_videos/apps.py
@@ -0,0 +1,6 @@
+from django.apps import AppConfig
+
+
+class AppVideosConfig(AppConfig):
+    default_auto_field = 'django.db.models.BigAutoField'
+    name = 'app_videos'
---
+++ django_csv_api/urls.py
@@ -0,0 +1,39 @@
+"""
+URL configuration for django_csv_api project.
+
+The `urlpatterns` list routes URLs to views. For more information please see:
+    https://docs.djangoproject.com/en/5.2/topics/http/urls/
+Examples:
+Function views
+    1. Add an import:  from my_app import views
+    2. Add a URL to urlpatterns:  path('', views.home, name='home')
+Class-based views
+    1. Add an import:  from other_app.views import Home
+    2. Add a URL to urlpatterns:  path('', Home.as_view(), name='home')
+Including another URLconf
+    1. Import the include() function: from django.urls import include, path
+    2. Add a URL to urlpatterns:  path('blog/', include('blog.urls'))
+"""
+from django.urls import path, include
+
+from drf_spectacular.views import (
+    SpectacularAPIView,
+    SpectacularRedocView,
+    SpectacularSwaggerView
+)
+
+from django_csv_api.views import index
+
+urlpatterns_v1 = [
+    path("api/", include([
+    ])),
+]
+
+urlpatterns = [
+    path("api/schema/", SpectacularAPIView.as_view(), name="schema"),
+    path("api/schema/swagger-ui/", SpectacularSwaggerView.as_view(url_name="schema"), name="swagger-ui"),
+    path("api/redoc/", SpectacularRedocView.as_view(url_name="schema"), name="redoc"),
+    path('', index, name="index"),
+]
+
+urlpatterns += urlpatterns_v1
